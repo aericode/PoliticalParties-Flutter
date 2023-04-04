345abc067e677277be5791f26389199587c82be3
Eric Andrade <eric.andrade.residente@tre-rn.jus.br>
2023-04-04 17:42:06 -0300

    finished

---
+++ lib/info.dart
@@ -0,0 +1,273 @@
+var info_array = [
+  [
+    "Movimento Democrático Brasileiro",
+    "MDB",
+    "15",
+    "2 075 721",
+    "15/01/1980",
+    "30/06/1981",
+    "Baleia Rossi"
+  ],
+  [
+    "Partido dos Trabalhadores",
+    "PT",
+    "13",
+    "1 607 378",
+    "10/02/1980",
+    "11/02/1982",
+    "Gleisi Hoffmann"
+  ],
+  [
+    "Partido da Social Democracia Brasileira",
+    "PSDB",
+    "45",
+    "1 326 875",
+    "25/06/1988",
+    "24/08/1988",
+    "Eduardo Leite"
+  ],
+  [
+    "Progressistas",
+    "PP",
+    "11",
+    "1 293 418",
+    "08/08/1995",
+    "16/11/1995",
+    "Ciro Nogueira"
+  ],
+  [
+    "Partido Democrático Trabalhista",
+    "PDT",
+    "12",
+    "1 125 294",
+    "17/06/1979",
+    "10/11/1981",
+    "André Figueiredo"
+  ],
+  [
+    "União Brasil",
+    "UNIÃO",
+    "44",
+    "1 058 386",
+    "06/10/2021",
+    "08/02/2022",
+    "Luciano Bivar"
+  ],
+  [
+    "Partido Trabalhista Brasileiro",
+    "PTB",
+    "14",
+    "1 053 424",
+    "21/11/1979",
+    "03/11/1981",
+    "Kassyo Santos Ramos"
+  ],
+  [
+    "Partido Liberal",
+    "PL",
+    "22",
+    "761 393",
+    "26/10/2006",
+    "19/12/2006",
+    "Valdemar Costa Neto"
+  ],
+  [
+    "Partido Socialista Brasileiro",
+    "PSB",
+    "40",
+    "627 255",
+    "02/07/1985",
+    "01/07/1988",
+    "Carlos Siqueira"
+  ],
+  [
+    "Republicanos",
+    "—",
+    "10",
+    "495 046",
+    "16/12/2003",
+    "25/08/2005",
+    "Marcos Pereira"
+  ],
+  [
+    "Cidadania",
+    "—",
+    "23",
+    "444 366",
+    "26/01/1992",
+    "19/03/1992",
+    "Roberto Freire"
+  ],
+  [
+    "Partido Social Cristão",
+    "PSC",
+    "20",
+    "407 134",
+    "15/05/1985",
+    "29/03/1990",
+    "Pastor Everaldo"
+  ],
+  [
+    "Podemos",
+    "PODE",
+    "19",
+    "405 134",
+    "01/05/1995",
+    "02/10/1997",
+    "Renata Abreu"
+  ],
+  [
+    "Partido Social Democrático",
+    "PSD",
+    "55",
+    "404 339",
+    "11/03/2011",
+    "27/09/2011",
+    "Gilberto Kassab"
+  ],
+  [
+    "Partido Comunista do Brasil",
+    "PCdoB",
+    "65",
+    "402 354",
+    "18/02/1962",
+    "23/07/1988",
+    "Luciana Santos"
+  ],
+  [
+    "Solidariedade",
+    "—",
+    "77",
+    "381 062",
+    "25/10/2012",
+    "24/09/2013",
+    "Paulinho da Força"
+  ],
+  [
+    "Partido Verde",
+    "PV",
+    "43",
+    "356 155",
+    "17/01/1986",
+    "30/09/1993",
+    "José Luiz Penna"
+  ],
+  [
+    "Patriota",
+    "—",
+    "51",
+    "326 456",
+    "09/08/2011",
+    "19/07/2012",
+    "Ovasco Resende"
+  ],
+  [
+    "Partido Socialismo e Liberdade",
+    "PSOL",
+    "50",
+    "225 965",
+    "07/07/2004",
+    "15/09/2005",
+    "Juliano Medeiros"
+  ],
+  ["Avante", "—", "70", "217 844", "15/05/1989", "11/10/1994", "Luis Tibé"],
+  [
+    "Partido da Mobilização Nacional",
+    "PMN",
+    "33",
+    "206 997",
+    "21/04/1984",
+    "25/10/1990",
+    "Antonio Massarollo"
+  ],
+  [
+    "Agir",
+    "—",
+    "36",
+    "194 119",
+    "11/07/1985",
+    "22/02/1990",
+    "Daniel Tourinho"
+  ],
+  [
+    "Democracia Cristã",
+    "DC",
+    "27",
+    "174 778",
+    "30/03/1995",
+    "05/08/1997",
+    "José Maria Eymael"
+  ],
+  [
+    "Partido Renovador Trabalhista Brasileiro",
+    "PRTB",
+    "28",
+    "145 178",
+    "27/11/1994",
+    "28/03/1995",
+    "Júlio Fidelix"
+  ],
+  [
+    "Partido da Mulher Brasileira",
+    "PMB",
+    "35",
+    "48 328",
+    "13/09/2008",
+    "29/09/2015",
+    "Suêd Haidar"
+  ],
+  [
+    "Rede Sustentabilidade",
+    "REDE",
+    "18",
+    "36 207",
+    "16/02/2013",
+    "22/09/2015",
+    "Heloísa Helena"
+  ],
+  [
+    "Partido Novo",
+    "NOVO",
+    "30",
+    "30 870",
+    "12/02/2011",
+    "15/09/2015",
+    "Eduardo Ribeiro"
+  ],
+  [
+    "Partido Socialista dos Trabalhadores Unificado",
+    "PSTU",
+    "16",
+    "15 298",
+    "05/06/1994",
+    "19/12/1995",
+    "Zé Maria"
+  ],
+  [
+    "Partido Comunista Brasileiro",
+    "PCB",
+    "21",
+    "12 523",
+    "25/03/1993",
+    "09/05/1996",
+    "Edmilson Costa"
+  ],
+  [
+    "Partido da Causa Operária",
+    "PCO",
+    "29",
+    "4 883",
+    "07/12/1995",
+    "30/07/1997",
+    "Rui Costa Pimenta"
+  ],
+  [
+    "Unidade Popular",
+    "UP",
+    "80",
+    "3 555",
+    "16/06/2016",
+    "10/12/2019",
+    "Leonardo Péricles"
+  ]
+];
